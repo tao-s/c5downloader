6e3bf052752a17705a99e8d8d242d3e59f42b564
Tao Sasaki <tao@xross-cube.com>
2015-01-14 05:56:44 +0900

    Update for 5.7.3.1

--- c5downloader.php
+++ c5downloader.php
@@ -6,10 +6,10 @@
  * コピーライト以外の改変、再配布OK。個人利用の範囲で勝手に使ってOKです。
  * その代わり当方では一切責任を負いません。
  */
-define("SRC_URL","http://www.concrete5.org/download_file/-/view/74252/");
-define("VERSION","Ver.5.7.3");
-define("FILENAME","./concrete5.5.7.3.zip");
-define("DIRNAME","./concrete5.7.3");
+define("SRC_URL","http://www.concrete5.org/download_file/-/view/74619/");
+define("VERSION","Ver.5.7.3.1");
+define("FILENAME","./concrete5.5.7.3.1.zip");
+define("DIRNAME","./concrete5.7.3.1");
 
 $messages = array(
     "ja" => array(
