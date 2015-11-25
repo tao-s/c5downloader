0b1cb641b8ebdc695c9259b39814ebe5d9748cec
Katz Ueno <katz@concrete5.co.jp>
2015-11-26 07:05:17 +0900

    Upgraded to 5.7.5.3

--- c5downloader.php
+++ c5downloader.php
@@ -13,10 +13,10 @@
  * コピーライト以外の改変、再配布OK。個人利用の範囲で勝手に使ってOKです。
  * その代わり当方では一切責任を負いません。
  */
-define("SRC_URL","http://www.concrete5.org/download_file/-/view/82243/");
-define("VERSION","Ver.5.7.5.2");
-define("FILENAME","./concrete5.7.5.2.zip");
-define("DIRNAME","./concrete5.7.5.2");
+define("SRC_URL","http://www.concrete5.org/download_file/-/view/84191/");
+define("VERSION","Ver.5.7.5.3");
+define("FILENAME","./concrete5.7.5.3.zip");
+define("DIRNAME","./concrete5.7.5.3");
 
 $messages = array(
     "ja" => array(
